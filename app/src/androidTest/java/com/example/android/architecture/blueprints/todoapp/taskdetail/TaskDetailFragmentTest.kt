863a66037583ffa1d45e39cd9e87fbdbb196e551
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-12-06 16:19:14 +0600

    Add integration test for TaskDetailFragment

---
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -0,0 +1,57 @@
+package com.example.android.architecture.blueprints.todoapp.taskdetail
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.ServiceLocator
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeAndroidTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+class TaskDetailFragmentTest {
+
+    private lateinit var repository: TasksRepository
+
+    @Before
+    fun initRepository() {
+        repository = FakeAndroidTasksRepository()
+        ServiceLocator.tasksRepository = repository
+    }
+
+    @After
+    fun resetRepository() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
+    @Test
+    fun activeTaskDetails_displayedInUi() = runBlockingTest {
+        // GIVEN active tasks added to DB
+        val activeTask = Task("Active Task", "AndroidX Rocks", false)
+        repository.saveTask(activeTask)
+
+        // WHEN details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
+        // THEN fragment displays one active task you saved
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText("Active Task")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText("AndroidX Rocks")))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isNotChecked()))
+    }
+}
