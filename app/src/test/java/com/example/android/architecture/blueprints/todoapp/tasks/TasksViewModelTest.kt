8d5ecfad719b5fb90af5bc5813672c4a54ba0f44
Valery Petrov <valeryvpetrov.dev@gmail.com>
2019-12-06 11:46:13 +0600

    Create TasksViewModel using constructor injection

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,29 +1,33 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTasksRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.Matchers.*
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Rule
-import org.junit.runner.RunWith
+import org.junit.Test
 
-@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var tasksRepository: FakeTasksRepository
+
     private lateinit var tasksViewModel: TasksViewModel
 
     @Before
     fun setup() {
+        tasksRepository = FakeTasksRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository.addTasks(task1, task2, task3)
         // GIVEN a fresh TasksViewModel
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        tasksViewModel = TasksViewModel(tasksRepository)
     }
 
     @Test
